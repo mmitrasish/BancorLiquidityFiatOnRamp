8dc8791802aaa55959c493d19295ab6943d44045
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2020-03-24 04:17:29 +0530

    Adds fetching liquidity token with its underlying tokens info

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import logo from "./logo.svg";
 import "./App.scss";
 import Widget from "./components/Widget";
 import Header from "./components/Header";
-import { getAllBancorLiquidityPoolTokens } from "./services/Web3Service";
+import {
+  getAllBancorLiquidityPoolTokens,
+  getConnectorTokenCount,
+  getConnectorTokens,
+  getSmartTokensOwner,
+  getErc20TokensInfo
+} from "./services/Web3Service";
 
 class App extends React.Component {
   state = {
@@ -21,8 +27,30 @@ class App extends React.Component {
       });
     }
   };
-  componentDidMount() {
-    getAllBancorLiquidityPoolTokens();
+  async componentDidMount() {
+    const allTokens = await getAllBancorLiquidityPoolTokens();
+    allTokens.forEach(async token => {
+      if (token) {
+        // console.log("Smart Token: ", token);
+        const ownerAddress = await getSmartTokensOwner(token);
+        // console.log("Owner Address: ", ownerAddress);
+        if (ownerAddress) {
+          const connectorTokenCount = await getConnectorTokenCount(
+            ownerAddress
+          );
+          for (let i = 0; i < connectorTokenCount; i++) {
+            const tokenAddress = await getConnectorTokens(ownerAddress, i);
+            const tokenInfo = await getErc20TokensInfo(tokenAddress);
+            console.log(tokenInfo);
+            // console.log("Token Address: " + tokenAddress);
+          }
+        }
+      }
+    });
+    const connectorTokenCount = getConnectorTokenCount();
+    for (let i = 0; i < connectorTokenCount; i++) {
+      getConnectorTokens(i);
+    }
   }
   render() {
     this.checkAddressChange();
--- src/services/Web3Service.js
+++ src/services/Web3Service.js
@@ -1,6 +1,9 @@
 import * as Web3 from "web3";
-import CONVERTER_REGISTRY_ABI from "../contracts/BancorConverterRegistry.json";
+import BANCOR_CONVERTER_REGISTRY_ABI from "../contracts/BancorConverterRegistry.json";
+import BANCOR_CONVERTER_ABI from "../contracts/BancorConverter.json";
 import CONTRACT_REGISTRY_ABI from "../contracts/ContractRegistry.json";
+import SMART_TOKEN_ABI from "../contracts/SmartToken.json";
+import ERC20_TOKEN_ABI from "../contracts/ERC20Token.json";
 
 const CONTRACT_REGISTRY_ADDRESS = "0x52Ae12ABe5D8BD778BD5397F99cA900624CfADD4";
 
@@ -27,33 +30,73 @@ export const getAccount = async () => {
 };
 
 export const getContractAddress = async contractName => {
-  let web3 = null;
-  try {
-    if (
-      typeof window.ethereum !== "undefined" ||
-      typeof window.web3 !== "undefined"
-    ) {
-      web3 = new Web3(window.ethereum);
-      const registry = new web3.eth.Contract(
-        CONTRACT_REGISTRY_ABI,
-        CONTRACT_REGISTRY_ADDRESS
-      );
-      const address = await registry.methods
-        .addressOf(web3.utils.fromAscii(contractName))
-        .call();
-      return address;
-    } else {
-      web3 = new Web3(
-        new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws")
-      );
-      window.readOnly = true;
-    }
-  } catch (error) {
-    console.log(error);
-  }
+  let web3 = web3Connector();
+  const registry = new web3.eth.Contract(
+    CONTRACT_REGISTRY_ABI,
+    CONTRACT_REGISTRY_ADDRESS
+  );
+  const address = await registry.methods
+    .addressOf(web3.utils.fromAscii(contractName))
+    .call();
+  return address;
 };
 
 export const getAllBancorLiquidityPoolTokens = async () => {
+  let web3 = web3Connector();
+  const BANCOR_CONVERTER_REGISTRY_ADDRESS = await getContractAddress(
+    "BancorConverterRegistry"
+  );
+  console.log(BANCOR_CONVERTER_REGISTRY_ADDRESS);
+  const contract = new web3.eth.Contract(
+    BANCOR_CONVERTER_REGISTRY_ABI,
+    BANCOR_CONVERTER_REGISTRY_ADDRESS
+  );
+  const poolTokens = await contract.methods.getSmartTokens().call();
+  return poolTokens;
+};
+
+export const getSmartTokensOwner = async smartTokenAddress => {
+  let web3 = web3Connector();
+  const contract = new web3.eth.Contract(SMART_TOKEN_ABI, smartTokenAddress);
+  const ownerAddress = await contract.methods.owner().call();
+  return ownerAddress;
+};
+
+export const getConnectorTokenCount = async tokenAddress => {
+  if (tokenAddress) {
+    let web3 = web3Connector();
+    const contract = new web3.eth.Contract(BANCOR_CONVERTER_ABI, tokenAddress);
+    const connectorTokenCount = await contract.methods
+      .connectorTokenCount()
+      .call();
+    return connectorTokenCount;
+  }
+};
+
+export const getConnectorTokens = async (tokenAddress, index) => {
+  if (tokenAddress) {
+    let web3 = web3Connector();
+    const contract = new web3.eth.Contract(BANCOR_CONVERTER_ABI, tokenAddress);
+    const connectorToken = await contract.methods.connectorTokens(index).call();
+    return connectorToken;
+  }
+};
+
+export const getErc20TokensInfo = async tokenAddress => {
+  if (tokenAddress) {
+    try {
+      let web3 = web3Connector();
+      const contract = new web3.eth.Contract(ERC20_TOKEN_ABI, tokenAddress);
+      const name = await contract.methods.name().call();
+      const symbol = await contract.methods.symbol().call();
+      return { name, symbol };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+const web3Connector = () => {
   let web3 = null;
   try {
     if (
@@ -61,16 +104,6 @@ export const getAllBancorLiquidityPoolTokens = async () => {
       typeof window.web3 !== "undefined"
     ) {
       web3 = new Web3(window.ethereum);
-      const CONVERTER_REGISTRY_ADDRESS = await getContractAddress(
-        "BancorConverterRegistry"
-      );
-      console.log(CONVERTER_REGISTRY_ADDRESS);
-      const contract = new web3.eth.Contract(
-        CONVERTER_REGISTRY_ABI,
-        CONVERTER_REGISTRY_ADDRESS
-      );
-      const poolTokens = await contract.methods.getSmartTokens().call();
-      console.log(poolTokens);
     } else {
       web3 = new Web3(
         new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws")
@@ -80,4 +113,5 @@ export const getAllBancorLiquidityPoolTokens = async () => {
   } catch (error) {
     console.log(error);
   }
+  return web3;
 };
