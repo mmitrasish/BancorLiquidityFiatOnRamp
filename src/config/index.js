8e79df6bbe71aa2714fc7413015667fd46ea5d5c
sanchaymittal <sanchaymittal@gmail.com>
2020-04-10 02:46:16 +0530

    merge

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 import { changeNetwork } from "../services/Web3Service";
+const process = require("dotenv").config();
 
 const networkConfig = {
   "1": {
@@ -6,20 +7,20 @@ const networkConfig = {
     onboardId: "052b3fe9-87d5-4614-b2e9-6dd81115979a",
     ethereumNodeEndpoint:
       "https://mainnet.infura.io/v3/55b4d27b09d64c4c8a6d9e381a51455d",
-    contractRegistryAddress: "0x52Ae12ABe5D8BD778BD5397F99cA900624CfADD4"
+    contractRegistryAddress: "0x52Ae12ABe5D8BD778BD5397F99cA900624CfADD4",
   },
   "3": {
     networkId: 3,
     onboardId: "052b3fe9-87d5-4614-b2e9-6dd81115979a",
     ethereumNodeEndpoint:
       "https://ropsten.infura.io/v3/55b4d27b09d64c4c8a6d9e381a51455d",
-    contractRegistryAddress: "0xFD95E724962fCfC269010A0c6700Aa09D5de3074"
-  }
+    contractRegistryAddress: "0xFD95E724962fCfC269010A0c6700Aa09D5de3074",
+  },
 };
 
 export let appConfig = networkConfig["1"];
 
-export const setAppConfig = networkId => {
+export const setAppConfig = (networkId) => {
   // console.log(networkId);
   appConfig = networkConfig[networkId + ""];
   changeNetwork(appConfig);
